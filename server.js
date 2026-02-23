998529888416e7d829092e1bdffa94e39d65201b
senamanana4-source <senamanana4@gmail.com>
2026-02-23 10:32:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -84,6 +84,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, ()=>{
-    console.log("Servidor en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log("Servidor iniciado en puerto " + PORT);
 });
